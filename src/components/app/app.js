0deb2c9783a57c206ae47b5bcf3cf8c18806eed3
olga dobrova <dobrova_o_g@mail.ru>
2024-02-28 15:58:40 +0300

    кнопка добавить в фос

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,27 +1,66 @@
-import "./app.css";
+import { Component } from "react";
+import nextId from "react-id-generator";
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
-function App() {
-  const data = [
-    { id: 0, name: "John C.", salary: 800, increase: false },
-    { id: 1, name: "Alex M.", salary: 3000, increase: true },
-    { id: 2, name: "Carl W.", salary: 9000, increase: false },
-  ];
-  return (
-    <div className="app">
-      <AppInfo />
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+import "./app.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { id: 0, name: "John C.", salary: 800, increase: false },
+        { id: 1, name: "Alex M.", salary: 3000, increase: true },
+        { id: 2, name: "Carl W.", salary: 9000, increase: false },
+      ],
+    };
+  }
+
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
+
+  addItem = (name, salary) => {
+    const elem = {
+      id: nextId(),
+      name: name,
+      salary: salary,
+      increase: false,
+    };
+
+    return (e) => {
+      e.preventDefault();
+      this.setState(({ data }) => {
+        const newArr = [...data, elem];
+        return {
+          data: newArr,
+        };
+      });
+    };
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <EmployeesList data={this.state.data} onDelete={this.deleteItem} />
+        <EmployeesAddForm onAdd={this.addItem} />
       </div>
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,27 +1,61 @@
 import "./employees-add-form.css";
+import { Component } from "react";
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавьте нового сотрудника</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="Как его зовут?"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="З/П в $?"
-        />
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      salary: "",
+    };
+  }
 
-        <button type="submit" className="btn btn-outline-light">
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
-};
+  onValueChange = (e) => {
+    // здесь берем вводимое в поле значение и записываем его в состояние, соответствующее атрибуту name в input
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+    const { onAdd } = this.props;
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Как его зовут?"
+            // атрибут name нужен для записи значения из поля input в нужное состояние (name)
+            name="name"
+            // это значение из состояния this.state
+            value={name}
+            onChange={this.onValueChange}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="З/П в $?"
+            // атрибут name нужен для записи значения из поля input в нужное состояние (salary)
+            name="salary"
+            // это значение из состояния this.state
+            value={salary}
+            onChange={this.onValueChange}
+          />
+
+          <button
+            type="submit"
+            className="btn btn-outline-light"
+            onClick={onAdd(name, salary)}
+          >
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployeesAddForm;
